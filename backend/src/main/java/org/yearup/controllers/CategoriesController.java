6dc7f2834cf7de1279d4e158d23a99e25d9781b5
sheku-is-a-se <sheku2026@gmail.com>
2026-06-22 22:02:26 -0400

    Add category API endpoints

--- backend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,8 @@
 package org.yearup.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -11,63 +10,86 @@ import org.yearup.service.ProductService;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@RestController
+@RequestMapping({"/categories"})
+@CrossOrigin
 public class CategoriesController
 {
-    private CategoryService categoryService;
-    private ProductService productService;
+    private CategoryService cs;
+    private ProductService ps;
+
+    public CategoriesController(CategoryService cs, ProductService ps) {
+        this.cs = cs;
+        this.ps = ps;
+    }
+
+
 
 
     // create an Autowired constructor to inject the categoryService and productService
 
     // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
+    @GetMapping
+    public List<Category> getAll(){
+
         // find and return all categories
-        return null;
+        return cs.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getById(@PathVariable int id) {
+        Category category = cs.getById(id);
         // get the category by id
-        return null;
+
+        if(category == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
+    @GetMapping("/{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+
+        return ps.listByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
+    @PostMapping
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Category> addCategory(@RequestBody Category category)
     {
+        Category addCategory = cs.create(category);
+
         // insert the category and return it with status 201 Created
-        return null;
+        return ResponseEntity.status(201).body(addCategory);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    public Category updateCategory(@PathVariable int id, @RequestBody Category category)
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Category> updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id and return the updated category (200 OK)
-        return null;
+        Category updated = cs.update(id, category);
+        return ResponseEntity.ok(updated);
     }
+
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteCategory(@PathVariable int id)
     {
         // delete the category by id and return status 204 No Content
-        return null;
+        cs.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
